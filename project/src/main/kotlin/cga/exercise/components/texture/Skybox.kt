54b379a2952bb8f02434947f829cf0f6bbb5a22a
Ylli Loshaj <y.loshaj@yahoo.de>
2022-06-23 18:15:22 +0200

    initial upload / Skybox added

---
+++ project/src/main/kotlin/cga/exercise/components/texture/Skybox.kt
@@ -0,0 +1,142 @@
+package cga.exercise.components.texture
+
+
+import cga.exercise.components.shader.ShaderProgram
+import org.joml.Matrix3f
+import org.joml.Matrix4f
+import org.lwjgl.BufferUtils
+import org.lwjgl.opengl.*
+import org.lwjgl.stb.STBImage
+import java.util.*
+import org.lwjgl.opengl.GL11
+import org.lwjgl.opengl.GL30.*
+
+class Skybox {
+
+    private var texID: Int = -1
+
+    private var skyBoxVao = 0
+    private var skyBoxVbo = 0
+    private var skyBoxIbo = 0
+
+    private var indexCount = 0
+
+
+    init {
+
+        var s = 1f
+
+        var skyboxVertices = floatArrayOf(
+            // Positions
+            -s, -s,  s,
+             s, -s,  s,
+             s, -s, -s,
+            -s, -s, -s,
+            -s,  s,  s,
+             s,  s,  s,
+             s,  s, -s,
+            -s,  s, -s
+        )
+
+        var skyboxIndices = intArrayOf(
+            // Right
+            1, 2, 6,
+            6, 5, 1,
+            // Left
+            0, 4, 7,
+            7, 3, 0,
+            // Top
+            4, 5, 6,
+            6, 7, 4,
+            // Bottom
+            0, 3, 2,
+            2, 1, 0,
+            // Back
+            0, 1, 5,
+            5, 4, 0,
+            // Front
+            3, 7, 6,
+            6, 2, 3
+        )
+
+        indexCount = skyboxIndices.size
+
+        // generate IDs
+        skyBoxVao = GL30.glGenVertexArrays()
+        skyBoxVbo = GL15.glGenBuffers()
+        skyBoxIbo = GL15.glGenBuffers()
+
+        // bind objects
+        GL30.glBindVertexArray(skyBoxVao)
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, skyBoxVbo)
+        GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, skyBoxIbo)
+
+        // upload mesh data
+        GL15.glBufferData(GL15.GL_ARRAY_BUFFER, skyboxVertices, GL15.GL_STATIC_DRAW)
+        GL20.glEnableVertexAttribArray(0)
+        GL20.glVertexAttribPointer(0, 3, GL11.GL_FLOAT, false, 12, 0)
+        GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, skyboxIndices, GL15.GL_STATIC_DRAW)
+
+        GL30.glBindVertexArray(0)
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0)
+        GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, 0)
+    }
+
+    fun applyTextures(faces : ArrayList<String>){
+        texID = GL11.glGenTextures()
+        GL11.glBindTexture(GL13.GL_TEXTURE_CUBE_MAP, texID)
+        //GL13.glEnable(AMDSeamlessCubemapPerTexture.GL_TEXTURE_CUBE_MAP_SEAMLESS)
+
+
+
+        for(i in 0 until faces.size)
+        {
+            var width = BufferUtils.createIntBuffer(1)
+            var height = BufferUtils.createIntBuffer(1)
+            var nrChannels = BufferUtils.createIntBuffer(1)
+
+            STBImage.stbi_set_flip_vertically_on_load(true)
+            val imageData = STBImage.stbi_load(faces[i], width, height, nrChannels, 4)
+                ?: throw Exception("Image file \"" + faces[i] + "\" couldn't be read:\n" + STBImage.stbi_failure_reason())
+
+
+            GL11.glTexImage2D(
+                GL13.GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
+                0, GL13.GL_RGBA8, width.get(), height.get(),
+                0, GL13.GL_RGBA, GL30.GL_UNSIGNED_BYTE, imageData)
+
+
+            STBImage.stbi_image_free(imageData)
+        }
+
+        GL11.glTexParameteri(GL13.GL_TEXTURE_CUBE_MAP, GL13.GL_TEXTURE_MAG_FILTER, GL13.GL_LINEAR)
+        GL11.glTexParameteri(GL13.GL_TEXTURE_CUBE_MAP, GL13.GL_TEXTURE_MIN_FILTER, GL13.GL_LINEAR)
+        GL11.glTexParameteri(GL13.GL_TEXTURE_CUBE_MAP, GL13.GL_TEXTURE_WRAP_S, GL13.GL_CLAMP_TO_EDGE)
+        GL11.glTexParameteri(GL13.GL_TEXTURE_CUBE_MAP, GL13.GL_TEXTURE_WRAP_T, GL13.GL_CLAMP_TO_EDGE)
+        GL11.glTexParameteri(GL13.GL_TEXTURE_CUBE_MAP, GL13.GL_TEXTURE_WRAP_R, GL13.GL_CLAMP_TO_EDGE)
+
+
+    }
+
+    fun render(skyBoxShader : ShaderProgram, view : Matrix4f, projection: Matrix4f){
+
+        //GL13.glEnable(GL_TEXTURE_CUBE_MAP_SEAMLESS)
+
+        glDepthFunc(GL_LEQUAL)
+
+        skyBoxShader.use()
+
+        val noTransView = Matrix4f(Matrix3f(view))
+
+        skyBoxShader.setUniform("view", noTransView , false)
+        skyBoxShader.setUniform("projection", projection, false)
+
+        GL30.glBindVertexArray(skyBoxVao)
+        GL30.glBindTexture(GL13.GL_TEXTURE_CUBE_MAP, texID)
+        GL11.glDrawElements(GL11.GL_TRIANGLES, indexCount, GL11.GL_UNSIGNED_INT, 0)
+
+        GL30.glBindVertexArray(0)
+
+        //GL11.glDepthMask(true)
+    }
+}
---
+++ project/src/main/kotlin/cga/exercise/game/Scene.kt
@@ -0,0 +1,216 @@
+package cga.exercise.game
+
+import cga.exercise.components.camera.TronCamera
+import cga.exercise.components.geometry.Material
+import cga.exercise.components.geometry.Mesh
+import cga.exercise.components.geometry.Renderable
+import cga.exercise.components.geometry.VertexAttribute
+import cga.exercise.components.light.PointLight
+import cga.exercise.components.light.SpotLight
+import cga.exercise.components.shader.ShaderProgram
+import cga.exercise.components.texture.Skybox
+import cga.exercise.components.texture.Texture2D
+import cga.framework.GLError
+import cga.framework.GameWindow
+import cga.framework.ModelLoader
+import cga.framework.OBJLoader
+import org.joml.Math
+import org.joml.Math.*
+import org.joml.Matrix4f
+import org.joml.Vector2f
+import org.joml.Vector3f
+import org.lwjgl.glfw.GLFW.*
+
+import org.lwjgl.opengl.GL30.*
+import kotlin.math.PI
+
+
+/**
+ * Created by Fabian on 16.09.2017.
+ */
+class Scene(private val window: GameWindow) {
+    private val staticShader: ShaderProgram
+    private val tronShader: ShaderProgram
+    private val skyBoxShader: ShaderProgram
+
+    private var currentShader: ShaderProgram
+
+    private val meshListSphere = mutableListOf<Mesh>()
+    private val meshListGround = mutableListOf<Mesh>()
+    val bodenmatrix: Matrix4f = Matrix4f()
+    val kugelMatrix: Matrix4f = Matrix4f()
+
+    val ground: Renderable
+    val sphere: Renderable
+    var cycle : Renderable
+
+    val camera = TronCamera()
+
+    val pointLight : PointLight
+    val spotLight: SpotLight
+    //MouseParam
+    var notFirstFrame = false
+    var oldMousePosX = 0.0
+    var oldMousePosY = 0.0
+
+    val skybox = Skybox()
+    val skyBoxFaces = arrayListOf<String>()
+
+
+
+
+    //scene setup
+    init {
+        staticShader = ShaderProgram("assets/shaders/simple_vert.glsl", "assets/shaders/simple_frag.glsl")
+        tronShader = ShaderProgram("assets/shaders/tron_vert.glsl", "assets/shaders/tron_frag.glsl")
+        skyBoxShader = ShaderProgram("assets/shaders/skyBox_vert.glsl", "assets/shaders/skyBox_frag.glsl")
+
+        currentShader = tronShader
+
+        skyBoxFaces.add("assets/textures/skybox/right.png")
+        skyBoxFaces.add("assets/textures/skybox/left.png")
+        skyBoxFaces.add("assets/textures/skybox/bottom.png")
+        skyBoxFaces.add("assets/textures/skybox/top.png")
+        skyBoxFaces.add("assets/textures/skybox/front.png")
+        skyBoxFaces.add("assets/textures/skybox/back.png")
+
+        skybox.applyTextures(skyBoxFaces)
+
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f); GLError.checkThrow()
+        glDisable(GL_CULL_FACE); GLError.checkThrow()
+        glFrontFace(GL_CCW)
+        glCullFace(GL_BACK)
+        glEnable(GL_DEPTH_TEST); GLError.checkThrow()
+        glDepthFunc(GL_LESS); GLError.checkThrow()
+
+
+        val objResSphere : OBJLoader.OBJResult = OBJLoader.loadOBJ("assets/models/sphere.obj")
+        val objMeshListSphere : MutableList<OBJLoader.OBJMesh> = objResSphere.objects[0].meshes
+
+        val objResGround : OBJLoader.OBJResult = OBJLoader.loadOBJ("assets/models/ground.obj")
+        val objMeshListGround : MutableList<OBJLoader.OBJMesh> = objResGround.objects[0].meshes
+
+        val stride = 8 * 4
+        val attrPos = VertexAttribute(3, GL_FLOAT, stride, 0)
+        val attrTC = VertexAttribute(2, GL_FLOAT, stride, 3 * 4)
+        val attrNorm = VertexAttribute(3, GL_FLOAT, stride, 5 * 4)
+
+        val vertexAttributes = arrayOf(attrPos,attrTC, attrNorm)
+
+        val groundEmitTexture = Texture2D("assets/textures/ground_emit.png", true)
+        val groundDiffTexture = Texture2D("assets/textures/ground_diff.png", true)
+        val groundSpecTexture = Texture2D("assets/textures/ground_spec.png", true)
+
+        groundEmitTexture.setTexParams(GL_REPEAT, GL_REPEAT, GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR)
+        groundDiffTexture.setTexParams(GL_REPEAT, GL_REPEAT, GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR)
+        groundSpecTexture.setTexParams(GL_REPEAT, GL_REPEAT, GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR)
+
+        val groundShininess = 60f
+        val groundTCMultiplier = Vector2f(64f)
+
+        val groundMaterial = Material(groundDiffTexture, groundEmitTexture, groundSpecTexture, groundShininess,
+                groundTCMultiplier)
+
+        for (mesh in objMeshListSphere) {
+            meshListSphere.add(Mesh(mesh.vertexData, mesh.indexData, vertexAttributes))
+        }
+
+        for (mesh in objMeshListGround) {
+            meshListGround.add(Mesh(mesh.vertexData, mesh.indexData, vertexAttributes, groundMaterial))
+        }
+
+        bodenmatrix.scale(0.03f)
+        bodenmatrix.rotateX(90f)
+
+        kugelMatrix.scale(0.5f)
+
+        ground = Renderable(meshListGround)
+        sphere = Renderable(meshListSphere)
+
+        camera.rotateLocal(Math.toRadians(-35f),0f, 0f)
+        camera.translateLocal(Vector3f(0f, 0f, 4f))
+        cycle = ModelLoader.loadModel("assets/light Cycle/light Cycle/HQ_Movie cycle.obj",
+                toRadians(-90f), toRadians(90f), 0f)?: throw Exception("Renderable can't be NULL!")
+
+        cycle.scaleLocal(Vector3f(0.8f))
+        camera.parent = cycle
+
+        pointLight = PointLight(Vector3f(0f, 2f, 0f), Vector3f(1f, 1f, 0f),
+                Vector3f(1f, 0.5f, 0.1f))
+
+        spotLight = SpotLight(Vector3f(0f, 1f, -2f), Vector3f(1f,1f,0.6f),
+                Vector3f(0.5f, 0.05f, 0.01f), Vector2f(toRadians(15f), toRadians(30f)))
+
+        spotLight.rotateLocal(toRadians(-10f), PI.toFloat(),0f)
+
+        pointLight.parent = cycle
+        spotLight.parent = cycle
+
+    }
+
+    fun render(dt: Float, t: Float) {
+
+        glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
+
+        skybox.render(skyBoxShader, camera.getCalculateViewMatrix(), camera.getCalculateProjectionMatrix())
+
+        currentShader.use()
+        camera.bind(currentShader)
+
+        spotLight.bind(currentShader, "spot", camera.getCalculateViewMatrix())
+        pointLight.bind(currentShader, "point")
+
+
+
+
+        currentShader.setUniform("farbe", Vector3f(abs(sin(t)), abs(sin(t/2f)), abs(sin(t/3f))))
+        cycle.render(currentShader)
+
+        currentShader.setUniform("farbe", Vector3f(0f,1f,0f))
+        ground.render(tronShader)
+
+    }
+
+    fun update(dt: Float, t: Float) {
+
+        pointLight.lightColor = Vector3f(abs(sin(t/3f)), abs(sin(t/4f)), abs(sin(t/2)))
+        //pointLight.lightColor = Vector3f(0.5f * sin(t) + 0.5f,0.5f * sin(t - 2/3 * PI.toFloat()) + 0.5f, 0.5f * sin(t - 5/3 * PI.toFloat()) + 0.5f)
+        when {
+            window.getKeyState(GLFW_KEY_W) -> {
+                if (window.getKeyState(GLFW_KEY_A)) {
+                    cycle.rotateLocal(0f,1.5f * dt,0f)
+                }
+                if (window.getKeyState(GLFW_KEY_D)) {
+                    cycle.rotateLocal(0f, 1.5f * -dt,0f)
+                }
+                cycle.translateLocal(Vector3f(0f, 0f, 15 * -dt))
+            }
+            window.getKeyState(GLFW_KEY_S) -> {
+                if (window.getKeyState(GLFW_KEY_A)) {
+                    cycle.rotateLocal(0f,1.5f * dt,0f)
+                }
+                if (window.getKeyState(GLFW_KEY_D)) {
+                    cycle.rotateLocal(0f, 1.5f * -dt,0f)
+                }
+                cycle.translateLocal(Vector3f(0f, 0f, 2f * dt))
+            }
+        }
+    }
+
+    fun onKey(key: Int, scancode: Int, action: Int, mode: Int) {}
+
+    fun onMouseMove(xpos: Double, ypos: Double) {
+        val deltaX = xpos - oldMousePosX
+        var deltaY = ypos - oldMousePosY
+        oldMousePosX = xpos
+        oldMousePosY = ypos
+
+        if(notFirstFrame) {
+            camera.rotateAroundPoint(0f, toRadians(deltaX.toFloat() * 0.05f), 0f, Vector3f(0f))
+        }
+
+        notFirstFrame = true
+    }
+
+    fun cleanup() {}
+}
