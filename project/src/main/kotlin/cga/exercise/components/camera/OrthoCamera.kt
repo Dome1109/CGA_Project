113e530f6ef34ad10c7b40da56394c61b68019c8
Ylli Loshaj <y.loshaj@yahoo.de>
2022-06-30 17:18:26 +0200

    orthographic camera added

---
+++ project/src/main/kotlin/cga/exercise/components/camera/OrthoCamera.kt
@@ -0,0 +1,24 @@
+package cga.exercise.components.camera
+
+import cga.exercise.components.geometry.Transformable
+import cga.exercise.components.shader.ShaderProgram
+import org.joml.Math
+import org.joml.Matrix4f
+
+class OrthoCamera (val width: Float = 16f, val height: Float = 9f, var nearPlane: Float = 0.1f,
+                   var farPlane: Float = 100f) : ICamera , Transformable(){
+    override fun getCalculateViewMatrix(): Matrix4f {
+        val view = Matrix4f()
+        return view.lookAt(getWorldPosition(), getWorldPosition().sub(getWorldZAxis()), getWorldYAxis())
+    }
+
+    override fun getCalculateProjectionMatrix(): Matrix4f {
+        var m = Matrix4f()
+        return m.orthoSymmetric(width, height , nearPlane, farPlane)
+    }
+
+    override fun bind(shader: ShaderProgram) {
+        shader.setUniform("view", getCalculateViewMatrix(), false)
+        shader.setUniform("projection", getCalculateProjectionMatrix(), false)
+    }
+}
